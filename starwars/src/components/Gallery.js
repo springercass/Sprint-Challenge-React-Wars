e36183aa2909e27fb283d092f9337c8bf8653959
springercass <casprin2@gmail.com>
2019-05-31 11:24:07 -0500

    mvp

---
+++ starwars/src/components/Gallery.js
@@ -0,0 +1,18 @@
+import React from "react";
+import StarwarsCharacter from "./StarwarsCharacter";
+
+class Gallery extends React.Component {
+  render() {
+    return (
+      <section className="gallery">
+        <div className="characters">
+          {this.props.galleryImages.map(character => (
+            <StarwarsCharacter character={character} />
+          ))}
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Gallery;
